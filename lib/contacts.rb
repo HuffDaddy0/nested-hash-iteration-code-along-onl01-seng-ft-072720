da22ef9e1ba2dbef757c2f01807e40f56da42155
Tyler Huffman <ty.huff24@gmail.com>
2020-07-31 17:02:53 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,6 +19,7 @@ def remove_strawberry(contacts)
     if contact == "Freddy Mercury"
       details_hash.each do |detail , data|
         if detail == ":favorite_ice_cream_flavors"
+          binding.pry
           data.delete_if do |ice_cream|
             ice_cream == "strawberry"
           end
